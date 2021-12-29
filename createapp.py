16ff5c2d80a9adb7806bfb022d85b9286ae0959d
JosephRidge <josephridge18@gmail.com>
2021-12-29 21:11:44 +0300

    Cleaner Code

--- createapp.py
+++ createapp.py
@@ -16,34 +16,6 @@ def mainApp():
     startAppQuestion = input("Would you like to create an app ? (y/n)  ")
     if (startAppQuestion == "y" or startAppQuestion == "Y" or  startAppQuestion =="yes"):
         print("\nYasssss lets do this ..... ")
-    #     newGithubRepoName = input("Kindly Enter your Repository Name : ")
-    #     newGithubRepoDescription = input("Kindly Enter your Project Description ( Brief ): ")
-    #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    #     driver.get("https://github.com/login")
-    #     # inputElement = driver.find_element_by_id("a1")
-    #     # inputElement.send_keys('1')
-    #     # print(secrets.myPassword)
-    #     myGithubEmail = driver.find_element(By.XPATH,'//*[@id="login_field"]')    
-    #     myGithubEmail.send_keys(secrets.myEmail)
-    #     myGithubPass = driver.find_element(By.XPATH,'//*[@id="password"]')    
-    #     myGithubPass.send_keys(secrets.myPassword)
-    #     myGithubButton = driver.find_element(By.XPATH,'//*[@id="login"]/div[4]/form/div/input[12]') 
-    #     myGithubButton.click()
-    # # new repo
-    #     driver.get("https://github.com/new")
-    #     newGithubRepositoryName = driver.find_element(By.XPATH, '//*[@id="repository_name"]')
-    #     newGithubRepositoryName.send_keys(newGithubRepoName)
-
-    #     # newGithubRepositoryDesc = driver.find_element_by_id('repository_description')
-    #     # newGithubRepositoryDesc.send_keys(newGithubRepositoryDesc)
-
-    #     repositoryAccessType = driver.find_element(By.XPATH,'//*[@id="repository_visibility_private"]')
-    #     repositoryAccessType.click()
-
-    #     createGithubRepo = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="new_repository"]/div[6]/button')))
-    #     createGithubRepo.click()
-    #     remoteREpoUrl = driver.find_element(By.XPATH, '//*[@id="empty-setup-new-repo-echo"]/span[6]/span')
-    #     print(f" \n\n {remoteREpoUrl.text}\n\n")
         os.chdir("/home/ridge/Documents")     
         print("\n_______________________________________\n")
         print("           Available Platforms \n_______________________________________\n \n 1. Vue3 Web Application. \n 2. Flutter Application. \n 3. Android Application. \n 4. Hardware Project (Arduino/ Platfrom.io etc) ")
@@ -62,10 +34,7 @@ def githubAccountVerfication(driver):
         githubOTPCode.send_keys(gitOtp)    
         githubOTPButton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="login"]/div[3]/form/button')))
         githubOTPButton.click() 
-        # newGithubRepositoryName = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH, '//*[@id="repository_name"]'))
-        # newGithubRepositoryName.send_keys(newGithubRepoName)
         
-
     else:
         print("Proceeding to github repo creation ....")    
     print("\n**********************************************************\n") 
@@ -74,10 +43,7 @@ def githubAccountVerfication(driver):
 
 def githubOperation(newGithubRepoName, githubPAT):
     print("setting up github .. ")
-    # newGithubRepoName = input("Kindly Enter your Repository Name : ")
-    # newGithubRepoDescription = input("Kindly Enter your Project Description ( Brief ): ")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # driver.implicitly_wait(10)
     driver.get("https://github.com/login")
     myGithubEmail = driver.find_element(By.XPATH,'//*[@id="login_field"]')    
     myGithubEmail.send_keys(secrets.myEmail)
@@ -86,22 +52,14 @@ def githubOperation(newGithubRepoName, githubPAT):
     myGithubButton = driver.find_element(By.XPATH,'//*[@id="login"]/div[4]/form/div/input[12]') 
     myGithubButton.click()
     print(f"==== > URL === > {driver.current_url}")
+
     if(driver.current_url == "https://github.com/sessions/verified-device"):
         githubAccountVerfication(driver) 
         
-        # newGithubRepositoryName = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH, '//*[@id="repository_name"]'))
-        # newGithubRepositoryName.send_keys(newGithubRepoName)
-    # driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    
-     # new repo
-
-    driver.execute_script("window.open('about:blank', 'secondtab');")
-      # It is switching to second tab now
-    driver.switch_to.window("secondtab") 
+    print(" --- - -- - - - - -- - -  > . > > .. > > ")
     driver.get("https://github.com/new")
-    # newGithubRepoPage = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH,'//*[@id="repos-container"]/h2/a'))
-    # newGithubRepoPage.click()
-  
+    newGithubRepositoryName = driver.find_element(By.XPATH, '//*[@id="repository_name"]')
+    newGithubRepositoryName.send_keys(newGithubRepoName)
     repositoryAccessType = driver.find_element(By.XPATH,'//*[@id="repository_visibility_private"]')
     repositoryAccessType.click()
     createGithubRepo = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="new_repository"]/div[6]/button')))
@@ -115,7 +73,6 @@ def githubOperation(newGithubRepoName, githubPAT):
     os.system(f"git remote add origin {remoteRepoUrl.text}")
     print(f"\n {githubPAT} \n")
     os.system(f"git push -u origin main ") 
-    # os.system(secrets.myEmail)
 
 def createVue3App():
     githubPAT = "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"
